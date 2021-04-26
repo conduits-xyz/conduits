5014543e3f178216891020a9b78fdb910d071640
Kavitha <kavitha@m5nv.com>
2021-04-26 17:42:43 +0530

    resource server: cleanup
    - remove comments
    - delete advanced spec

--- backend/crud-server/src/routes/validate.js
+++ backend/crud-server/src/routes/validate.js
@@ -9,9 +9,6 @@ const { RestApiError } = require('../../../lib/error');
 /// `onError` is a http status code to be returned on error
 function validate({ schema, path, onError }) {
   async function middleware(req, res, next) {
-    // console.log('!!!!!!!!!!!!', schema, path);
-    // do something with schema
-    // console.log('>>> print payload:', req.body[path]);
     const payload = req.body[path] ?? {};
     try {
       const validated = await schema.validate(payload, {
@@ -19,29 +16,12 @@ function validate({ schema, path, onError }) {
         strict: true,
       });
       res.locals.validatedBody = { [path]: validated };
-
-      // console.log('1.0 ~~~~~~~~~~~', res.locals.validatedBody);
-      // if (path === 'conduit') {
-      //   if (_ignore.description !== 'ignore me') {
-      //     console.log('~~~ request-validity: ', validated);
-      //   }
-      // }
     } catch (errors) {
       const validationErrors = [];
-      // let displayErrors = false;
-      // if (req.method === 'POST' /*&& path === 'user'*/) {
-      //   console.log('~~~~', req.method, req.body);
-      //   displayErrors = true;
-      // }
       for (const error of errors.inner) {
-        // if (displayErrors) {
-        //   console.log('~~~~~~~~~', error.path, error.errors[0]);
-        // }
         validationErrors.push({ [error.path]: error.errors[0] });
       }
       if (onError) {
-        // return next(new RestApiError(onError, {later: "I promise"}));
-        // console.log(validationErrors);
         return next(new RestApiError(onError, validationErrors));
       }
     }
